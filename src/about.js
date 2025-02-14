d608f90f8f37892431fbc1866f7f21ed03c3cec3
saicastic <saiprithvisuperior@gmail.com>
2025-02-14 09:34:10 +0530

    fix

---
+++ src/about.js
@@ -0,0 +1,11 @@
+import gsap from "gsap";
+
+document.querySelector(".nobutton").addEventListener("mouseover", function () {
+  let x = Math.floor(Math.random() * (250 - -250 + 1)) + -250;
+  let y = Math.floor(Math.random() * (250 - -250 + 1)) + -250;
+  gsap.to(".nobutton", {
+    x,
+    y,
+    duration: 0.1,
+  });
+});
